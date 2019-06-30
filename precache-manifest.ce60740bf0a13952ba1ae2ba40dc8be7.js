480184e98ba9a0112f6a1229622af76195a6efd8
Rodion Chachura <geekrodion@gmail.com>
2019-06-30 17:05:26 +0300

    Updates

---
+++ precache-manifest.ce60740bf0a13952ba1ae2ba40dc8be7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d554a48935b73a09e278450c135b6d",
+    "url": "/breakout-game/index.html"
+  },
+  {
+    "revision": "73bfe7f975f95271e1ca",
+    "url": "/breakout-game/static/css/main.fd668050.chunk.css"
+  },
+  {
+    "revision": "c119dc81b5b0d90f8ec9",
+    "url": "/breakout-game/static/js/2.2a3097d4.chunk.js"
+  },
+  {
+    "revision": "73bfe7f975f95271e1ca",
+    "url": "/breakout-game/static/js/main.689c0549.chunk.js"
+  },
+  {
+    "revision": "15f8e20d4366c01b7a33",
+    "url": "/breakout-game/static/js/runtime~main.5546415a.js"
+  }
+]);

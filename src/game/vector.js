1b3597298372a195a69208b845ac97bbfb6c7261
Rodion Chachura <geekrodion@gmail.com>
2019-07-04 08:48:55 +0300

    feature: core logic

--- src/game/vector.js
+++ src/game/vector.js
@@ -1,3 +1,5 @@
+import { toDegrees, toRadians } from '../utils'
+
 export default class Vector {
   constructor(x, y) {
     this.x = x
@@ -18,5 +20,43 @@ export default class Vector {
 
   normalize() {
     return this.scaleBy(1 / this.length())
+  }
+
+  subtract({ x, y }) {
+    return new Vector(this.x - x, this.y - y)
+  }
+
+  dotProduct({ x, y }) {
+    return this.x * x + this.y * y
+  }
+
+  projectOn(other) {
+    const amt = this.dotProduct(other) / other.length()
+    return new Vector(amt * other.x, amt * other.y)
+  }
+
+  reflect(normal) {
+    return this.subtract(this.projectOn(normal).scaleBy(2))
+  }
+
+  rotate(degrees) {
+    const radians = toRadians(degrees)
+    const cos = Math.cos(radians)
+    const sin = Math.sin(radians)
+
+    return new Vector(
+      this.x * cos - this.y * sin,
+      this.x * sin + this.y * cos
+    )
+  }
+
+  crossProduct({ x, y }) {
+    return this.x * y - x * this.y
+  }
+
+  angleBetween(other) {
+    return toDegrees(
+      Math.atan2(this.crossProduct(other), this.dotProduct(other))
+    )
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,11 @@
+export const toDegrees = radians => (radians * 180) / Math.PI
+export const toRadians = degrees => (degrees * Math.PI) / 180
+
 export const getRange = length => [...Array(length).keys()]
 export const getRandomFrom = (...args) => args[Math.floor(Math.random() * args.length)]
 export const flatten = arrays => arrays.reduce((acc, row) => [...acc, ...row], [])
+export const withoutElement = (array, element) => array.filter(e => e !== element)
+export const updateElement = (array, oldElement, newElement) => array.map(e => e === oldElement ? newElement : e)
 
 export const registerListener = (eventName, handler) => {
   window.addEventListener(eventName, handler)
